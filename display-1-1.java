56e1ef471f19021d70e8e18bc352bfe2ee8cf633
Ricardo Barbosa <ric3639@gmail.com>
2019-10-01 07:15:58 -0700

    section 1.1-pt1

---
+++ display-1-1.java
@@ -0,0 +1,14 @@
+public class FirstProgram
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello reader");
+        System.out.println("Welcome to Java");
+
+        System.out.println("Lets demo a simple calc");
+        
+        int ans;
+        ans = 2+2;
+        System.out.println("2 plus 2 is: " + ans);
+    }
+}
